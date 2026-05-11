bf4bc9e8d4d0cd04812a4540de67138f711b1989
snoopytime68 <144932681+snoopytime68@users.noreply.github.com>
2026-05-11 16:17:40 +0000

    Added an inventory system to keep track of the eggs you collected.

--- main.py
+++ main.py
@@ -1,33 +1,46 @@
 # Imports
 import os
-os.system('cls' if os.name == 'nt' else 'clear')
+
 import random
 import time
 # Variables
 
+# This function clears the screen and clears it on start up
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+clear_terminal()
 # This is the Brains of the game. The terminal clears and updates once
 # every second
 def main():
     x = 0
+    inventory = []
     while True:
         print("option 1: collect an egg")
         print()
         print("option 2: inventory")
         user_input = input("choose an option ")
-
+        # If the user inputs 1 than this code will play out
         if user_input == "1":
+            clear_terminal()
             user_input_name = input("Make a name for your dragon egg! ")
             user_input_color = input("what does it look like? ")
-
             rand_size = random.randint(1,5)
 
-            Egg(user_input_name, user_input_color, rand_size)
-
+            new_egg = Egg(user_input_name, user_input_color, rand_size)
+            inventory.append(new_egg)
+            time.sleep(3)
+       
+        # If the user enters 2 than this code will play out
         if user_input == "2":
-            pass
+            clear_terminal()
+            for egg in inventory:
+                print(f"{egg.name} | {egg.health}")
+            input("press enter to continue")
+            clear_terminal()            
+            
 
         time.sleep(1)
-        os.system('cls' if os.name == 'nt' else 'clear')
+        clear_terminal()
         
 class Egg:
     def __init__(self, name, color, size):
@@ -39,6 +52,6 @@ class Egg:
         self.incubation = 0
 
         print(f"You collected {self.name}, it is {self.color}, and it size {self.size}")
-        time.sleep(5)
+        
 
 main()
